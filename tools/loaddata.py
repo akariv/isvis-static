76fddc86a2f4b7046b10992c3fb64a9df5a672da
Camila <camila.labarca@ug.uchile.cl>
2022-04-05 08:52:04 -0400

    script that loads data from airtable

---
+++ tools/loaddata.py
@@ -0,0 +1,28 @@
+import json
+import requests
+import sys
+import os
+
+os.environ['AIRTABLE_APIKEY']=  'keyDrtBN4iiDAF2Nm'
+base_id = 'app72pKh6npOppDZY'
+api_key = os.getenv('AIRTABLE_APIKEY')
+headers = {'Authorization': 'Bearer '  + api_key}
+def get_airtable_data(table_name):
+    resp = []
+    url = "https://api.airtable.com/v0/" + base_id + "/" + table_name
+    response = requests.get(url, headers=headers)
+    airtable_response = response.json()
+    airtable_response = airtable_response['records']
+    for elem in airtable_response:
+        new_dict = {'id': elem['id'], 'createdTime': elem['createdTime']}
+        new_dict.update(elem['fields'])
+        resp.append(new_dict)
+    str_response = json.dumps(resp)
+    str_response = 'var '+table_name+'=\n'+str_response[:-1] +'\n];'
+    return str_response
+
+
+
+with open('../js/'+sys.argv[1]+'.js', 'w') as f:
+    f.write(get_airtable_data(sys.argv[1]))
+    f.close()
