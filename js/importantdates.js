7342cd2ca19f7398077c1a96e8b57c9b60c953e8
camilabarca <camila.labarca@ug.uchile.cl>
2022-04-13 16:22:19 +0000

    Automatic update of web site

--- js/importantdates.js
+++ js/importantdates.js
@@ -1,6 +1,3 @@
-
-var importantdates = [
-{"_id":{"$oid":"5c0b62a8290d52416bb9bce3"},"dateTitle":"Open Call: launch","dateLabel":"Open Call: launch","dateTime":"Jan 28th, 2019","createdAt":{"$date":"2018-12-08T06:20:24.949Z"},"updatedAt":{"$date":"2019-01-27T13:24:00.759Z"},"__v":0,"id":"5c0b62a8290d52416bb9bce3","isNegative":false,"isPast":false},
-{"_id":{"$oid":"5c0b62ca290d52416bb9bce4"},"dateTitle":"Open Call: Deadline","dateLabel":"Open Call: Deadline","dateTime":"Mar 9th, 2019","createdAt":{"$date":"2018-12-08T06:20:58.015Z"},"updatedAt":{"$date":"2019-02-28T20:24:04.984Z"},"__v":0,"id":"5c0b62ca290d52416bb9bce4","isNegative":false,"isPast":false},
-{"_id":{"$oid":"5c0b62d8290d52416bb9bce5"},"dateTitle":"ISVIS 2019","dateLabel":"ISVIS 2019","dateTime":"May 12th, 2019","createdAt":{"$date":"2018-12-08T06:21:12.768Z"},"updatedAt":{"$date":"2019-01-27T13:24:47.192Z"},"__v":0,"id":"5c0b62d8290d52416bb9bce5","isNegative":true,"isPast":false}
+var importantdates=
+[{"id": "recO6qbOQQBBqMdth", "createdTime": "2022-04-08T13:36:08.000Z", "dateTitle": "Open Call: launch", "dateLabel": "Open Call: launch", "dateTime": "Jan 28th, 2019", "__v": 0, "isPast": "False"}, {"id": "rectNocKxyMTKUmYa", "createdTime": "2022-04-08T13:36:08.000Z", "dateTitle": "Open Call: Deadline", "dateLabel": "Open Call: Deadline", "dateTime": "Mar 9th, 2019", "__v": 0, "isPast": "False"}, {"id": "recYeyi0m5VNHuw0Y", "createdTime": "2022-04-08T13:36:08.000Z", "dateTitle": "ISVIS 2019", "dateLabel": "ISVIS 2019", "dateTime": "May 12th, 2019", "__v": 0, "isNegative": true, "isPast": "False"}
 ];
