0a50838af34c73d5cd4050d54fd51aacb94a9c66
camilabarca <camila.labarca@ug.uchile.cl>
2023-04-03 11:09:46 +0000

    Automatic update of web site

--- js/tickettype.js
+++ js/tickettype.js
@@ -1,3 +1,3 @@
 var tickettype=
-[
+[{"id": "recbJon1CsRi6FOhV", "createdTime": "2022-04-14T06:41:13.000Z", "title": "Conference Ticket", "meta-title": "10% Early-Bird discount by May 10th", "price": "180 NIS", "__v": 0, "link": "https://hodaot.haifa.ac.il/pay/PaymentForISVISForm.asp", "price-desc": "complimentary lunch & coffee included", "include": true}, {"id": "recvjpMWbk15x6UHe", "createdTime": "2023-04-03T11:03:30.000Z", "title": "Conference Ticket (online)", "price": "150 NIS", "meta-title": "17% Early-Bird discount by May 10th", "link": "https://hodaot.haifa.ac.il/pay/PaymentForISVISForm.asp", "include": true, "__v": 0}, {"id": "recTAkW9vhfXmp50r", "createdTime": "2023-04-03T11:07:49.000Z", "link": "https://hodaot.haifa.ac.il/pay/PaymentForISVISForm.asp", "price-desc": "complimentary lunch & coffee included", "title": "Student Ticket", "meta-title": "20% Early-Bird discount by May 10th", "price": "80 NIS (show student card)", "include": true, "__v": 0}
 ];
